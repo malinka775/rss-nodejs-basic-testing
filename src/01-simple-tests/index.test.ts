71efd149d8a6fada939228361575a410b422b763
Alina <malinka775@yandex.ru>
2024-10-17 20:39:31 +0300

    test: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,86 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+
+const ARRAY_A = [-1000, -157, -39, -3, 0, 8, 89, 100, 1290];
+const ARRAY_B = [-1111, -200, -25, -9, 0, 10, 199, 1389];
+const INVALID_ARGS = [
+  'ahj',
+  '245',
+  { number: 2 },
+  () => {
+    return;
+  },
+  [1],
+];
+
+const ARRAY_VALID_ACTIONS = Object.values(Action);
+const ARRAY_INVALID_ACTIONS = ['++', '=', 'str', ')', '('];
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    ARRAY_A.forEach((a) => {
+      ARRAY_B.forEach((b) => {
+        const result = simpleCalculator({ a, b, action: Action.Add });
+        expect(result).toBe(a + b);
+      });
+    });
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    ARRAY_A.forEach((a) => {
+      ARRAY_B.forEach((b) => {
+        const result = simpleCalculator({ a, b, action: Action.Subtract });
+        expect(result).toBe(a - b);
+      });
+    });
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    ARRAY_A.forEach((a) => {
+      ARRAY_B.forEach((b) => {
+        const result = simpleCalculator({ a, b, action: Action.Multiply });
+        expect(result).toBe(a * b);
+      });
+    });
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    ARRAY_A.forEach((a) => {
+      ARRAY_B.forEach((b) => {
+        const result = simpleCalculator({ a, b, action: Action.Divide });
+        expect(result).toBe(a / b);
+      });
+    });
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    ARRAY_A.forEach((a) => {
+      ARRAY_B.forEach((b) => {
+        const result = simpleCalculator({ a, b, action: Action.Exponentiate });
+        expect(result).toBe(a ** b);
+      });
+    });
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    ARRAY_A.forEach((a) => {
+      ARRAY_B.forEach((b) => {
+        ARRAY_INVALID_ACTIONS.forEach((action) => {
+          const result = simpleCalculator({ a, b, action });
+          expect(result).toBeNull;
+        });
+      });
+    });
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    INVALID_ARGS.forEach((a) => {
+      ARRAY_B.forEach((b) => {
+        ARRAY_VALID_ACTIONS.forEach((action) => {
+          const result = simpleCalculator({ a, b, action });
+          expect(result).toBeNull;
+        });
+      });
+    });
   });
 });
